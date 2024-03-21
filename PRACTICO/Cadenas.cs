fc292260063ed39733c8d4f95547eb475a8d2440
CarlosEdgar27 <carlitoselmejor817@gmail.com>
2024-03-21 09:54:49 -0400

    CadenasEjercicio1
    Hice el Ejercicio1, y puse mi nombre en el .txt

--- PRACTICO/Cadenas.cs
+++ PRACTICO/Cadenas.cs
@@ -3,7 +3,13 @@ namespace Practico
 {
     public class Cadenas
     {
-        
-    
+        static void Ejercicio1() //volver a revisar despues :u
+        {
+            Console.WriteLine("EJERCICIO 1: Introduzca una oracion: ");
+            string cadena = Console.ReadLine();
+            string[] palabras = cadena.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Cantidad de palabras: {palabras.Length}");
+            Console.ReadLine();
+        }
     }
 }
