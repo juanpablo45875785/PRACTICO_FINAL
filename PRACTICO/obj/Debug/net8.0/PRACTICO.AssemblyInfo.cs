60f2878b2c88b19441f4a255dd412b7f8062bbac
programacion <jtapeosichamo>
2024-03-30 14:33:15 -0400

    otra fusion

--- PRACTICO/obj/Debug/net8.0/PRACTICO.AssemblyInfo.cs
+++ PRACTICO/obj/Debug/net8.0/PRACTICO.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRACTICO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb6ee117632bab8cae1bc75f27d4ea6640fb7cc4")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ec839300cdf1df96222f01eebe04cc5026c720")]
->>>>>>> Danny
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07948303cf2318697544304df580230c88fee573")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRACTICO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRACTICO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
