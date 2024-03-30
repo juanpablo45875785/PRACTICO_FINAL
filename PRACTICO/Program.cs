0fc74ea133cc71bc468db9c8e4d047af97ce9c2f
programacion <jtapeosichamo>
2024-03-30 15:01:48 -0400

    ULTIMA MODIFICACIONES

--- PRACTICO/Program.cs
+++ PRACTICO/Program.cs
@@ -6,18 +6,23 @@ namespace Practico
 
     static void Main (string[] args)
     {
+//       NUMEROS
 
-         
+            Numeros.Ejercicio1();
+            Numeros.Ejercicio2();
+            Numeros.Ejercicio3();
+            Numeros.Ejercicio4();
+            Numeros.Ejercicio5();
 
+//       SERIES
 
-        
-        Matrices matrices = new Matrices();
-            matrices.Ejercicio1();
-            matrices.Ejercicio2();
-            matrices.Ejercicio3();
-            matrices.Ejercicio4();
-            matrices.Ejercicio5();
-            
+            Series.Ejercicio1();
+            Series.Ejercicio2();
+            Series.Ejercicio3();
+            Series.Ejercicio4();
+            Series.Ejercicio5();
+
+//       CADENAS
 
             Cadenas.Ejercicio1();
             Cadenas.Ejercicio2();
@@ -25,29 +30,22 @@ namespace Practico
             Cadenas.Ejercicio4();
             Cadenas.Ejercicio5();
 
-            Numeros.Ejercicio1();
-            Numeros.Ejercicio2();
-            Numeros.Ejercicio3();
-            Numeros.Ejercicio4();
-            Numeros.Ejercicio5();
-            
-
-
+//       VECTORES
     
-
             Vectores.ejercicio1();
             Vectores.ejercicio2();
             Vectores.ejercicio3();
             Vectores.ejercicio4();
             Vectores.ejercicio5();
 
-    
-          
-            Series.Ejercicio1();
-            Series.Ejercicio2();
-            Series.Ejercicio3();
-            Series.Ejercicio4();
-            Series.Ejercicio5();
+//       MATRICES
+
+        Matrices matrices = new Matrices();
+            matrices.Ejercicio1();
+            matrices.Ejercicio2();
+            matrices.Ejercicio3();
+            matrices.Ejercicio4();
+            matrices.Ejercicio5();
 
         }
     }
